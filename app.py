e8b6bd6b0ee252af9866865d9b1c894d58f6c499
natthidaonnavong <natthidaonnavong@gmail.com>
2026-03-21 20:04:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,38 @@
 import streamlit as st
 
-st.write("Hello, Streamlit! iii")
+st.set_page_config(page_title="SuperSmart", layout="wide")
+
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+
+def go_to(page):
+    st.session_state.page = page
+
+# HOME PAGE
+if st.session_state.page == "home":
+    st.title("SuperSmart")
+    st.write("Choose a module")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("SmartCount")
+        if st.button("Open SmartCount", use_container_width=True):
+            go_to("smartcount")
+            st.rerun()
+
+    with col2:
+        st.subheader("SmartCast")
+        if st.button("Open SmartCast", use_container_width=True):
+            go_to("smartcast")
+            st.rerun()
+
+# SMARTCOUNT PAGE
+elif st.session_state.page == "smartcount":
+    import pages.smartcount as smartcount
+    smartcount.render(go_to)
+
+# SMARTCAST PAGE
+elif st.session_state.page == "smartcast":
+    import pages.smartcast as smartcast
+    smartcast.render(go_to)
